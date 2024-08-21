3e3235a528b3bc712b08deba5cb528b0d136196c
someshshewalkar <someshwarshewalkar@gmail.com>
2024-08-21 09:36:22 +0530

    user database model add

---
+++ model/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(`mongodb://localhost:27017/authentication`)
+
+const userSchema = mongoose.Schema({
+    username : String,
+    email : String,
+    password : String,
+    age : Number
+})
+
+module.exports = mongoose.model("users",userSchema)
+
